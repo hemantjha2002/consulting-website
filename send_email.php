525a9fccb910e361396f9f04723efa9df7c652d9
Hemant Jha <92109901+hemantjha2002@users.noreply.github.com>
2023-08-28 14:31:11 +0530

    UI changes made and php email setuped.

--- send_email.php
+++ send_email.php
@@ -12,78 +12,56 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $recipient = "hemsthecoder@gmail.com"; // Change this to your email address
     $subject = "New Contact Form Submission";
     $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $contact = $_POST["contact"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
-    // Create a PHPMailer instance
+    $userMail = new PHPMailer(true);
+
+    try {
+        $userMail->isSMTP();
+        $userMail->Host = 'smtp.gmail.com'; 
+        $userMail->SMTPAuth = true;
+        $userMail->Username = 'hemsthecoder@gmail.com';
+        $userMail->Password = 'xqpuzjeioeajlatk';
+        $userMail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS
+        $userMail->Port = 587;
+
+        $userMail->setFrom('hemsthecoder@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); // Change this
+        $userMail->addAddress($email);
+        
+        $userMail->isHTML(true);
+        $userMail->Subject = "Thank You for Contacting Us";
+        $userMail->Body = "Dear $firstName,<br><br>We have received your contact form submission. Thank you for getting in touch with us.<br><br>Best regards,<br>Your Company NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD.";
+
+        $userMail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
     $mail = new PHPMailer(true);
 
     try {
-        // Server settings
         $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';  // Change this to your SMTP server
+        $mail->Host = 'smtp.gmail.com'; 
         $mail->SMTPAuth = true;
         $mail->Username = 'hemsthecoder@gmail.com';
         $mail->Password = 'xqpuzjeioeajlatk';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
         $mail->Port = 587;
 
-        // Recipients
-        $mail->setFrom('hemsthecoder@gmail.com', 'Your Company'); // Change this
+        
+        $mail->setFrom('hemsthecoder@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); 
         $mail->addAddress($recipient);
-        $mail->addReplyTo($email, $firstName);
-
-        // Content
+        
         $mail->isHTML(false);
-        $mail->Subject = $subject;
-        $mail->Body = "Name: $firstName\nEmail: $email\n\n$message";
+        $mail->Subject = "Contact from submission from website";
+        $mail->Body = "Name: $firstName $lastname\nEmail: $email\n Contact Number: $contact\n Message:$message\n";
 
         $mail->send();
-        header("Location: thank_you.html"); // Redirect to thank you page
+        header("Location:thank_you.html"); 
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 ?>
-
-<!-- 
-extract($_POST);
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
-
-try {
-    Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtp.gmail.com';                     
-    $mail->SMTPAuth   = true;                                  
-    $mail->Password   = 'xqpuzjeioeajlatk';                        
-    $mail->Port       = 465;        
-    
-    $mail->setFrom('hemsthecoder@gmail.com', 'Mailer');
-    $mail->addAddress($email, 'Joe User');     
-    $mail->addAddress('ellen@example.com');              
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
-
-    
-    $mail->isHTML(true);                                 
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-?> -->
