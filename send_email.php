6bf8fea4dee746bd5f34da11b5a412daf5ef9ecc
Hemant Jha <92109901+hemantjha2002@users.noreply.github.com>
2023-08-29 16:28:17 +0530

    Gallery updated

--- send_email.php
+++ send_email.php
@@ -9,35 +9,36 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recipient = "hemsthecoder@gmail.com"; // Change this to your email address
+    $recipient = "naisha1crclub@gmail.com"; // Change this to your email address
     $subject = "New Contact Form Submission";
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
     $contact = $_POST["contact"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $userMail = new PHPMailer(true);
+   
+    // $userMail = new PHPMailer(true);
 
-    try {
-        $userMail->isSMTP();
-        $userMail->Host = 'smtp.gmail.com'; 
-        $userMail->SMTPAuth = true;
-        $userMail->Username = 'hemsthecoder@gmail.com';
-        $userMail->Password = 'xqpuzjeioeajlatk';
-        $userMail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS
-        $userMail->Port = 587;
+    // try {
+    //     $userMail->isSMTP();
+    //     $userMail->Host = 'smtp.gmail.com'; 
+    //     $userMail->SMTPAuth = true;
+    //     $userMail->Username = 'naisha1crclub@gmail.com';
+    //     $userMail->Password = 'iqmxbxiclhrlhkcg';
+    //     $userMail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS
+    //     $userMail->Port = 587;
 
-        $userMail->setFrom('hemsthecoder@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); // Change this
-        $userMail->addAddress($email);
+    //     $userMail->setFrom('naisha1crclub@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); // Change this
+    //     $userMail->addAddress($email);
         
-        $userMail->isHTML(true);
-        $userMail->Subject = "Thank You for Contacting Us";
-        $userMail->Body = "Dear $firstName,<br><br>We have received your contact form submission. Thank you for getting in touch with us.<br><br>Best regards,<br> NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD.";
+    //     $userMail->isHTML(true);
+    //     $userMail->Subject = "Thank You for Contacting Us";
+    //     $userMail->Body = "Dear $firstName,<br><br>We have received your contact form submission. Thank you for getting in touch with us.<br><br>Best regards,<br> NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD.";
 
-        $userMail->send();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+    //     $userMail->send();
+    // } catch (Exception $e) {
+    //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    // }
 
     $mail = new PHPMailer(true);
 
@@ -45,13 +46,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; 
         $mail->SMTPAuth = true;
-        $mail->Username = 'hemsthecoder@gmail.com';
-        $mail->Password = 'xqpuzjeioeajlatk';
+        $mail->Username = 'naisha1crclub@gmail.com';
+        $mail->Password = 'iqmxbxiclhrlhkcg';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
         $mail->Port = 587;
 
         
-        $mail->setFrom('hemsthecoder@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); 
+        $mail->setFrom('naisha1crclub@gmail.com', "NAISHA'S 1CR CLUB CONSULTING SERVICES PVT. LTD."); 
         $mail->addAddress($recipient);
         
         $mail->isHTML(false);
